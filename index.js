f829f5fb92d59605a15dbd3d698181e3f5a0b76e
Izzynaruto! <joelbuluma@gmail.com>
2025-11-23 16:51:11 +0300

    Updated results display logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//are the string array elements the same one in the HTML? 
+//Variables
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
@@ -7,8 +7,9 @@ const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0;
 let computerScore = 0;
-//where has the playerChoice declared as a parameter?
+
 function playGame(playerChoice) {
+
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     let result = "";
 
@@ -28,19 +29,29 @@ function playGame(playerChoice) {
                 break;
         }
     }
+
+    //Results display logic
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultsDisplay.textContent = result;
     switch (result) {
         case "YOU WIN":
             resultsDisplay.classList.add("greenText");
+            resultsDisplay.classList.remove("redText");
             playerScore++;
             playerScoreDisplay.textContent = playerScore;
             break;
         case "YOU LOSE":
             resultsDisplay.classList.add("redText");
+            resultsDisplay.classList.remove("greenText");
             computerScore++;
             computerScoreDisplay.textContent = computerScore;
             break;
+        case "TIE!":
+            resultsDisplay.classList.remove("redText");
+            resultsDisplay.classList.remove("greenText");
+            break;
     }
+
 }
+
